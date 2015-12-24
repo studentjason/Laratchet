c5a7caef3ebfbba1ff24ea030b7a0db98302c960
Jason <jason@trend-tec.com.tw>
2015-12-24 17:07:34 +0800

    ratchet 的  websocket 服務

---
+++ app/Websocket/Chat.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Websocket;
+
+use Ratchet\MessageComponentInterface;
+use Ratchet\ConnectionInterface;
+use Illuminate\Support\Facades\Redis;
+
+use Exception;
+
+class Chat implements MessageComponentInterface {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
+    public function onOpen(ConnectionInterface $conn) {
+        try {
+            // Store the new connection to send messages to later
+            $this->clients->attach($conn);
+            $conn->send('welcome');
+            Log::v('S', $conn, 'welcome');
+
+            $request = (array)$conn->WebSocket->request;
+            $request = (array)array_get($request, "\0*\0headers");
+            $request = (array)array_get($request, "\0*\0headers.user-agent");
+            $request = array_get($request, "\0*\0values.0");
+            $request = (empty($request)) ? 'unknown' : $request;
+            Log::v('R', $conn, "new client({$conn->resourceId}) on {$conn->remoteAddress}({$request})");
+        } catch (Exception $e) {
+            Log::e($e);
+        }
+    }
+
+    public function onMessage(ConnectionInterface $from, $msg) {
+        try {
+            Log::v('R', $from, "receiving message \"{$msg}\"");
+            $numRecv = count($this->clients) - 1;
+            foreach ($this->clients as $client) {
+                if ($from !== $client) {
+                    // The sender is not the receiver, send to each client connected
+                    $client->send($msg);
+                    Log::v('S', $client, "sending message \"{$msg}\"");
+                }
+            }
+        } catch (Exception $e) {
+            Log::e($e);
+        }
+    }
+
+    public function onClose(ConnectionInterface $conn) {
+        try {
+            // The connection is closed, remove it, as we can no longer send it messages
+            $this->clients->detach($conn);
+
+            Log::v('R', $conn, 'close', "Client({$conn->resourceId}) has disconnected");
+        } catch (Exception $e) {
+            Log::e($e);
+        }
+    }
+
+    public function onError(ConnectionInterface $conn, \Exception $e) {
+        Log::e($e);
+        $conn->close();
+    }
+}
